5b34a947d3737b81246a5b42c1dec21d91e2c1f4
MaxPat931 <86370342+MaxPat931@users.noreply.github.com>
2022-04-04 12:47:09 -0700

    Create ClericNames.js

---
+++ ClericNames.js
@@ -0,0 +1,302 @@
+"advancement": [			
+	{		
+	"_id": 	"M668Fpykz43jnKiB",	
+	"type": 	"ItemGrant",	
+	"configuration": 	{	
+		"items": 	[
+			Spellcasting (Cleric), Divine Domain, Additional Cleric Spells
+			]
+		},	
+		"value": 	{},
+		"level": 	1,
+		"title": 	"Features",
+		"icon": 	"",
+		"classRestriction": 	""
+	},		
+	{		
+	"_id": 	"DHGQaKaI9Jb8oYQj",	
+	"type": 	"ItemGrant",	
+	"configuration": 	{	
+		"items": 	[
+			Bane, Bless, Command, Create or Destroy Water, Cure Wounds, Detect Evil and Good, Detect Magic, Detect Poison and Disease, Guiding Bolt, Healing Word, Inflict Wounds, Protection from Evil and Good, Purify Food and Drink, Sanctuary, Shield of Faith
+			]
+		},	
+		"value": 	{},
+		"level": 	1,
+		"title": 	"Spells - 1st Level",
+		"icon": 	"",
+		"classRestriction": 	""
+	},		
+	{		
+	"_id": 	"N9ry34SDl6tKOfyT",	
+	"type": 	"ItemGrant",	
+	"configuration": 	{	
+		"items": 	[
+			Channel Divinity (Cleric), Channel Divinity: Turn Undead, Harness Divine Power
+			]
+		},	
+		"value": 	{},
+		"level": 	2,
+		"title": 	"Features",
+		"icon": 	"",
+		"classRestriction": 	""
+	},		
+	{		
+	"_id": 	"Zn86DIjPgTY4JOTJ",	
+	"type": 	"ItemGrant",	
+	"configuration": 	{	
+		"items": 	[
+			Aid, Augury, Blindness/Deafness, Calm Emotions, Continual Flame, Enhance Ability, Find Traps, Gentle Repose, Hold Person, Lesser Restoration, Locate Object, Prayer of Healing, Protection from Poison, Silence, Spiritual Weapon, Warding Bond, Zone of Truth, Borrowed Knowledge
+			]
+		},	
+		"value": 	{},
+		"level": 	3,
+		"title": 	"Spells - 2nd Level",
+		"icon": 	"",
+		"classRestriction": 	""
+	},		
+	{		
+	"_id": 	"VpBpj0uPddur6bS6",	
+	"type": 	"ItemGrant",	
+	"configuration": 	{	
+		"items": 	[
+			Cantrip Versatility
+			]
+		},	
+		"value": 	{},
+		"level": 	4,
+		"title": 	"Optional Features",
+		"icon": 	"",
+		"classRestriction": 	""
+	},		
+	{		
+	"_id": 	"32htnvT6D9FgiGOQ",	
+	"type": 	"ItemGrant",	
+	"configuration": 	{	
+		"items": 	[
+			Destroy Undead
+			]
+		},	
+		"value": 	{},
+		"level": 	5,
+		"title": 	"Features",
+		"icon": 	"",
+		"classRestriction": 	""
+	},		
+	{		
+	"_id": 	"ZrWSzp6i7x7N0cRF",	
+	"type": 	"ItemGrant",	
+	"configuration": 	{	
+		"items": 	[
+			Animate Dead, Beacon of Hope, Bestow Curse, Clairvoyance, Create Food and Water, Daylight, Dispel Magic, Feign Death, Glyph of Warding, Magic Circle, Mass Healing Word, Meld into Stone, Protection from Energy, Remove Curse, Revivify, Sending, Speak with Dead, Spirit Guardians, Tongues, Water Walk, 
+			Aura of vitality, Spirit shroud
+			]
+		},	
+		"value": 	{},
+		"level": 	5,
+		"title": 	"Spells - 3rd Level",
+		"icon": 	"",
+		"classRestriction": 	""
+	},		
+	{		
+	"_id": 	"67EfScqRi1QmMafw",	
+	"type": 	"ItemGrant",	
+	"configuration": 	{	
+		"items": 	[
+			
+			]
+		},	
+		"value": 	{},
+		"level": 	6,
+		"title": 	"Channel Divinity Uses: 2 per Rest",
+		"icon": 	"",
+		"classRestriction": 	""
+	},		
+	{		
+	"_id": 	"cB4MpfPOWgaXVPLn",	
+	"type": 	"ItemGrant",	
+	"configuration": 	{	
+		"items": 	[
+			Banishment, Control Water, Death Ward, Divination, Freedom of Movement, Guardian of Faith, Locate Creature, Stone Shape, 
+			Aura of life, Aura of purity
+			]
+		},	
+		"value": 	{},
+		"level": 	7,
+		"title": 	"Spells - 4th Level",
+		"icon": 	"",
+		"classRestriction": 	""
+	},		
+	{		
+	"_id": 	"0niQnrhqGYCDqnmI",	
+	"type": 	"ItemGrant",	
+	"configuration": 	{	
+		"items": 	[
+			
+			]
+		},	
+		"value": 	{},
+		"level": 	8,
+		"title": 	"Destroy Undead - CR1",
+		"icon": 	"",
+		"classRestriction": 	""
+	},		
+	{		
+	"_id": 	"d0r1NGoxnkpkVOgx",	
+	"type": 	"ItemGrant",	
+	"configuration": 	{	
+		"items": 	[
+			Commune, Contagion, Dispel Evil and Good, Flame Strike, Geas, Greater Restoration, Hallow, Insect Plague, Legend Lore, Mass Cure Wounds, Planar Binding, Raise Dead, Scrying, 
+			Summon celestial
+			]
+		},	
+		"value": 	{},
+		"level": 	9,
+		"title": 	"Spells - 5th Level",
+		"icon": 	"",
+		"classRestriction": 	""
+	},		
+	{		
+	"_id": 	"Bx9Fe4GfcpPj9EdX",	
+	"type": 	"ItemGrant",	
+	"configuration": 	{	
+		"items": 	[
+			Divine Intervention
+			]
+		},	
+		"value": 	{},
+		"level": 	10,
+		"title": 	"Features",
+		"icon": 	"",
+		"classRestriction": 	""
+	},		
+	{		
+	"_id": 	"gPr6PTdk1YoGH8n1",	
+	"type": 	"ItemGrant",	
+	"configuration": 	{	
+		"items": 	[
+			Blade Barrier, Create Undead, Find the Path, Forbiddance, Harm, Heal, Heroes' Feast, Planar Ally, True Seeing, Word of Recall, 
+			Sunbeam
+			]
+		},	
+		"value": 	{},
+		"level": 	11,
+		"title": 	"Features",
+		"icon": 	"",
+		"classRestriction": 	""
+	},		
+	{		
+	"_id": 	"T9voXIUp2Rnxec9P",	
+	"type": 	"ItemGrant",	
+	"configuration": 	{	
+		"items": 	[
+			
+			]
+		},	
+		"value": 	{},
+		"level": 	11,
+		"title": 	"Destroy Undead - CR 2",
+		"icon": 	"",
+		"classRestriction": 	""
+	},		
+	{		
+	"_id": 	"phO3ibYWyjlGlXEV",	
+	"type": 	"ItemGrant",	
+	"configuration": 	{	
+		"items": 	[
+			Conjure Celestial, Divine Word, Etherealness, Fire Storm, Plane Shift, Regenerate, Resurrection, Symbol
+			]
+		},	
+		"value": 	{},
+		"level": 	13,
+		"title": 	"Spells - 7th Level",
+		"icon": 	"",
+		"classRestriction": 	""
+	},		
+	{		
+	"_id": 	"0Fs7hAuyanx0QGEy",	
+	"type": 	"ItemGrant",	
+	"configuration": 	{	
+		"items": 	[
+			
+			]
+		},	
+		"value": 	{},
+		"level": 	14,
+		"title": 	"Destroy Undead - CR 3",
+		"icon": 	"",
+		"classRestriction": 	""
+	},		
+	{		
+	"_id": 	"CMzXeFE7xjkDhY7o",	
+	"type": 	"ItemGrant",	
+	"configuration": 	{	
+		"items": 	[
+			Antimagic Field, Control Weather, Earthquake, Holy Aura, 
+			Sunburst
+			]
+		},	
+		"value": 	{},
+		"level": 	15,
+		"title": 	"Spells - 8th Level",
+		"icon": 	"",
+		"classRestriction": 	""
+	},		
+	{		
+	"_id": 	"uz8c9lD1Utrrq9m0",	
+	"type": 	"ItemGrant",	
+	"configuration": 	{	
+		"items": 	[
+			
+			]
+		},	
+		"value": 	{},
+		"level": 	17,
+		"title": 	"Destroy Undead - CR 4",
+		"icon": 	"",
+		"classRestriction": 	""
+	},		
+	{		
+	"_id": 	"HxD7sfVn7yMipQkS",	
+	"type": 	"ItemGrant",	
+	"configuration": 	{	
+		"items": 	[
+			Astral Projection, Gate, Mass Heal, True Resurrection, 
+			Power word heal
+			]
+		},	
+		"value": 	{},
+		"level": 	17,
+		"title": 	"Spells - 9th Level",
+		"icon": 	"",
+		"classRestriction": 	""
+	},		
+	{		
+	"_id": 	"TFI1q6HEDDcIdlxM",	
+	"type": 	"ItemGrant",	
+	"configuration": 	{	
+		"items": 	[
+			
+			]
+		},	
+		"value": 	{},
+		"level": 	18,
+		"title": 	"Channel Divinity - 3/Rest",
+		"icon": 	"",
+		"classRestriction": 	""
+	},		
+	{		
+	"_id": 	"fGxdf3SS1NFjVDGC",	
+	"type": 	"ItemGrant",	
+	"configuration": 	{	
+		"items": 	[
+			
+			]
+		},	
+		"value": 	{},
+		"level": 	20,
+		"title": 	"Divine Intervention - Automatic Success",
+		"icon": 	"",
+		"classRestriction": 	""
+	}		
+],			
