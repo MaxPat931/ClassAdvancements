4d7b5aaac79dda00219aa3de925b65b3880915d0
MaxPat931 <86370342+MaxPat931@users.noreply.github.com>
2022-04-04 12:46:30 -0700

    Create ClericAdv.js

---
+++ ClericAdv.js
@@ -0,0 +1,302 @@
+"advancement": [			
+	{		
+	"_id": 	"SFQU2y0rn18asAWb",	
+	"type": 	"ItemGrant",	
+	"configuration": 	{	
+		"items": 	[
+			"Compendium.dnd5e.classfeatures.v4gKwLhAq9vuqza7", "Compendium.dnd5e.classfeatures.x637K2Icp2ZFM1TB"
+			]
+		},	
+		"value": 	{},
+		"level": 	1,
+		"title": 	"Features",
+		"icon": 	"",
+		"classRestriction": 	""
+	},		
+	{		
+	"_id": 	"ismovBYbJJxiIipJ",	
+	"type": 	"ItemGrant",	
+	"configuration": 	{	
+		"items": 	[
+			"Compendium.dnd5e.spells.95K2aUhAGV9qXjnf", "Compendium.dnd5e.spells.8dzaICjGy6mTUaUr", "Compendium.dnd5e.spells.arzCrMRgcNiQuh43", "Compendium.dnd5e.spells.a3XtAO5n2GrqiAh5", "Compendium.dnd5e.spells.uUWb1wZgtMou0TVP", "Compendium.dnd5e.spells.Mzh95utKDPIrjiH8", "Compendium.dnd5e.spells.ghXTfe7sgCbgf1Q8", "Compendium.dnd5e.spells.2skfDtglk1mGrb3l", "Compendium.dnd5e.spells.7buEm5KhI5lP8m1z", "Compendium.dnd5e.spells.o8Dh7fblk1d16tnO", "Compendium.dnd5e.spells.ksaaTxIbKx2sJfia", "Compendium.dnd5e.spells.xmDBqZhRVrtLP8h2", "Compendium.dnd5e.spells.Kn7K5PtYUJAKZTTp", "Compendium.dnd5e.spells.gvdA9nPuWLck4tBl", "Compendium.dnd5e.spells.jZ6JNykRtdQ90MOo"
+			]
+		},	
+		"value": 	{},
+		"level": 	1,
+		"title": 	"Spells - 1st Level",
+		"icon": 	"",
+		"classRestriction": 	""
+	},		
+	{		
+	"_id": 	"h1aRvZm1vskXSpHJ",	
+	"type": 	"ItemGrant",	
+	"configuration": 	{	
+		"items": 	[
+			"Compendium.dnd5e.classfeatures.YpiLQEKGalROn7iJ", "Compendium.dnd5e.classfeatures.r91UIgwFdHwkXdia"
+			]
+		},	
+		"value": 	{},
+		"level": 	2,
+		"title": 	"Features",
+		"icon": 	"",
+		"classRestriction": 	""
+	},		
+	{		
+	"_id": 	"VMprUSwqlrYbasNR",	
+	"type": 	"ItemGrant",	
+	"configuration": 	{	
+		"items": 	[
+			"Compendium.dnd5e.spells.Opwh2PdX4runSBlm", "Compendium.dnd5e.spells.4v2H3hHb3Ph9XLNd", "Compendium.dnd5e.spells.zwGsAv6kmwzYGhh3", "Compendium.dnd5e.spells.3MYDjS6k9IYL0aTj", "Compendium.dnd5e.spells.MK6gpQMeDFo0cP9f", "Compendium.dnd5e.spells.9eOZDBImVKxbeOyZ", "Compendium.dnd5e.spells.KrM3oHVv13RAALrS", "Compendium.dnd5e.spells.n4JDcFKe5ikzYmAc", "Compendium.dnd5e.spells.3Lo9boi7P2ro6QV4", "Compendium.dnd5e.spells.F0GsG0SJzsIOacwV", "Compendium.dnd5e.spells.SleYkHovQ8NagmeV", "Compendium.dnd5e.spells.MOEmz9N0j0QPkKEE", "Compendium.dnd5e.spells.MAxM77CDUu8dgIRQ", "Compendium.dnd5e.spells.5VhqFROQYjr1P9lp", "Compendium.dnd5e.spells.JbxsYXxSOTZbf9I0", "Compendium.dnd5e.spells.JVhKeanAXZH62DrF", "Compendium.dnd5e.spells.CylBa7jR8DSbo8Z3"
+			]
+		},	
+		"value": 	{},
+		"level": 	3,
+		"title": 	"Spells - 2nd Level",
+		"icon": 	"",
+		"classRestriction": 	""
+	},		
+	{		
+	"_id": 	"7a4Nf71vn3HcYt5l",	
+	"type": 	"ItemGrant",	
+	"configuration": 	{	
+		"items": 	[
+			
+			]
+		},	
+		"value": 	{},
+		"level": 	4,
+		"title": 	"Optional Features",
+		"icon": 	"",
+		"classRestriction": 	""
+	},		
+	{		
+	"_id": 	"FA2ZwbhseO7BEPsh",	
+	"type": 	"ItemGrant",	
+	"configuration": 	{	
+		"items": 	[
+			"Compendium.dnd5e.classfeatures.NMy4piwXIpLjYbRE"
+			]
+		},	
+		"value": 	{},
+		"level": 	5,
+		"title": 	"Features",
+		"icon": 	"",
+		"classRestriction": 	""
+	},		
+	{		
+	"_id": 	"XBC2wLRA0MXkXCW9",	
+	"type": 	"ItemGrant",	
+	"configuration": 	{	
+		"items": 	[
+			"Compendium.dnd5e.spells.oyE5nVppa5mde5gT", "Compendium.dnd5e.spells.ZU9d6woBdUP8pIPt", "Compendium.dnd5e.spells.pO4zGe5LmFIYqJiL", "Compendium.dnd5e.spells.cg50KpBkBdPK6vPL", "Compendium.dnd5e.spells.BV0mpbHh29IbbIj5", "Compendium.dnd5e.spells.BP3GCwa66IAw1yTG", "Compendium.dnd5e.spells.15Fa6q1nH27XfbR8", "Compendium.dnd5e.spells.pB7XVYwdGNcUG935", "Compendium.dnd5e.spells.y8A4HfTwd93ypdEz", "Compendium.dnd5e.spells.34ddoYIrnOZ2GFYi", "Compendium.dnd5e.spells.64uo4fHriHLjRUrX", "Compendium.dnd5e.spells.j8NtLXOOJ3GAKF8I", "Compendium.dnd5e.spells.XZhdgVK3cLoxNCQl", "Compendium.dnd5e.spells.LmRHHMtplpxr9fX6", "Compendium.dnd5e.spells.GtGjNjPBgUHxGYAD", "Compendium.dnd5e.spells.I2LUSF5ogc7Bj62e", "Compendium.dnd5e.spells.uCud2s4TjMfjiXUb", "Compendium.dnd5e.spells.gopnZvS0c2jD5FP8", "Compendium.dnd5e.spells.YBda6nLKjxdT1LbS"
+			
+			]
+		},	
+		"value": 	{},
+		"level": 	5,
+		"title": 	"Spells - 3rd Level",
+		"icon": 	"",
+		"classRestriction": 	""
+	},		
+	{		
+	"_id": 	"2mX8gyQuSOHvS9Ah",	
+	"type": 	"ItemGrant",	
+	"configuration": 	{	
+		"items": 	[
+			
+			]
+		},	
+		"value": 	{},
+		"level": 	6,
+		"title": 	"Channel Divinity Uses: 2 per Rest",
+		"icon": 	"",
+		"classRestriction": 	""
+	},		
+	{		
+	"_id": 	"mENPKXOdslmDUmh9",	
+	"type": 	"ItemGrant",	
+	"configuration": 	{	
+		"items": 	[
+			"Compendium.dnd5e.spells.pxpb2eOB6bv4phAf", "Compendium.dnd5e.spells.7fFHlBk3UNX8gPKL", "Compendium.dnd5e.spells.VtCXMdyM6mAdIJZb", "Compendium.dnd5e.spells.XqzXSKNR75ZdYTA9", "Compendium.dnd5e.spells.da0a1t2FqaTjRZGT", "Compendium.dnd5e.spells.TgHsuhNasPbhu8MO", "Compendium.dnd5e.spells.gXtzz9t1DTzJeLr4", "Compendium.dnd5e.spells.QvGcdRUSNRKEQJlK"
+			
+			]
+		},	
+		"value": 	{},
+		"level": 	7,
+		"title": 	"Spells - 4th Level",
+		"icon": 	"",
+		"classRestriction": 	""
+	},		
+	{		
+	"_id": 	"kTKP8NgzJ1IOWzP4",	
+	"type": 	"ItemGrant",	
+	"configuration": 	{	
+		"items": 	[
+			
+			]
+		},	
+		"value": 	{},
+		"level": 	8,
+		"title": 	"Destroy Undead - CR1",
+		"icon": 	"",
+		"classRestriction": 	""
+	},		
+	{		
+	"_id": 	"386S1bW4pYE8SXZl",	
+	"type": 	"ItemGrant",	
+	"configuration": 	{	
+		"items": 	[
+			"Compendium.dnd5e.spells.d54VDyFulD9xxY7J", "Compendium.dnd5e.spells.CjIq8Ed7bu3vVwT1", "Compendium.dnd5e.spells.TkJ8Wtg1L7TZtspm", "Compendium.dnd5e.spells.5e1xTohkzqFqbYH4", "Compendium.dnd5e.spells.JQyigMNPiDnGI18b", "Compendium.dnd5e.spells.WzvJ7G3cqvIubsLk", "Compendium.dnd5e.spells.SLxA9QhrggTz0taU", "Compendium.dnd5e.spells.OVikYmSdHliAG2YD", "Compendium.dnd5e.spells.W4Qx5z0id6da0vqg", "Compendium.dnd5e.spells.Pyzmm8R7rVsNAPsd", "Compendium.dnd5e.spells.42O2aNBW7vK90gTL", "Compendium.dnd5e.spells.AGFMPAmuzwWO6Dfz", "Compendium.dnd5e.spells.fVbCxFRaORalHB20"
+			
+			]
+		},	
+		"value": 	{},
+		"level": 	9,
+		"title": 	"Spells - 5th Level",
+		"icon": 	"",
+		"classRestriction": 	""
+	},		
+	{		
+	"_id": 	"QfpfeoSvrbSfzgoG",	
+	"type": 	"ItemGrant",	
+	"configuration": 	{	
+		"items": 	[
+			"Compendium.dnd5e.classfeatures.eVXqHn0ojWrEuYGU"
+			]
+		},	
+		"value": 	{},
+		"level": 	10,
+		"title": 	"Features",
+		"icon": 	"",
+		"classRestriction": 	""
+	},		
+	{		
+	"_id": 	"5EtVyfIoZVjJFfqr",	
+	"type": 	"ItemGrant",	
+	"configuration": 	{	
+		"items": 	[
+			"Compendium.dnd5e.spells.dLJhxDfeyOsc3zsY", "Compendium.dnd5e.spells.E4NXux0RHvME1XgP", "Compendium.dnd5e.spells.cYI4RNNjI5GAmLhy", "Compendium.dnd5e.spells.64D1gNZ4hx7SWG2x", "Compendium.dnd5e.spells.tMH6Ivn4GmE1naMj", "Compendium.dnd5e.spells.qcYitWoSMTnKkzM1", "Compendium.dnd5e.spells.mgFqi0ev8f7Ut19y", "Compendium.dnd5e.spells.fkREcytuZ8sngWtC", "Compendium.dnd5e.spells.XzkJpE6XpZfKjODD", "Compendium.dnd5e.spells.76C0FdcxlU8F9Rl2"
+			
+			]
+		},	
+		"value": 	{},
+		"level": 	11,
+		"title": 	"Features",
+		"icon": 	"",
+		"classRestriction": 	""
+	},		
+	{		
+	"_id": 	"LtSiaVcTK72Rtrdt",	
+	"type": 	"ItemGrant",	
+	"configuration": 	{	
+		"items": 	[
+			
+			]
+		},	
+		"value": 	{},
+		"level": 	11,
+		"title": 	"Destroy Undead - CR 2",
+		"icon": 	"",
+		"classRestriction": 	""
+	},		
+	{		
+	"_id": 	"Sh4huDQLBKFlJsCq",	
+	"type": 	"ItemGrant",	
+	"configuration": 	{	
+		"items": 	[
+			"Compendium.dnd5e.spells.XT7nzJmVGgv73uaf", "Compendium.dnd5e.spells.T1vpZLam7LezjToj", "Compendium.dnd5e.spells.PQuEgKyCdovOvhqN", "Compendium.dnd5e.spells.J3uILDYS7MiOfmTJ", "Compendium.dnd5e.spells.J6Jpw5XzB5aTeqnz", "Compendium.dnd5e.spells.9kGrFXnLiRg3Xnbo", "Compendium.dnd5e.spells.jhhT9PsHy5A7EojO", "Compendium.dnd5e.spells.B2kbmgbA2WQR00kx"
+			]
+		},	
+		"value": 	{},
+		"level": 	13,
+		"title": 	"Spells - 7th Level",
+		"icon": 	"",
+		"classRestriction": 	""
+	},		
+	{		
+	"_id": 	"W5Mh4Uf7VJCSo2lC",	
+	"type": 	"ItemGrant",	
+	"configuration": 	{	
+		"items": 	[
+			
+			]
+		},	
+		"value": 	{},
+		"level": 	14,
+		"title": 	"Destroy Undead - CR 3",
+		"icon": 	"",
+		"classRestriction": 	""
+	},		
+	{		
+	"_id": 	"dr9C6LWjy5VzUJKi",	
+	"type": 	"ItemGrant",	
+	"configuration": 	{	
+		"items": 	[
+			"Compendium.dnd5e.spells.Eon0jzGzQRNluTPQ", "Compendium.dnd5e.spells.ZPd73HtKF3At11jh", "Compendium.dnd5e.spells.x5JNBSyIBBZsjcGT", "Compendium.dnd5e.spells.j8S49Rea8b1640Zi"
+			
+			]
+		},	
+		"value": 	{},
+		"level": 	15,
+		"title": 	"Spells - 8th Level",
+		"icon": 	"",
+		"classRestriction": 	""
+	},		
+	{		
+	"_id": 	"fgsYp33rrAfc0hyN",	
+	"type": 	"ItemGrant",	
+	"configuration": 	{	
+		"items": 	[
+			
+			]
+		},	
+		"value": 	{},
+		"level": 	17,
+		"title": 	"Destroy Undead - CR 4",
+		"icon": 	"",
+		"classRestriction": 	""
+	},		
+	{		
+	"_id": 	"yS9XbAjvn1RAiNez",	
+	"type": 	"ItemGrant",	
+	"configuration": 	{	
+		"items": 	[
+			"Compendium.dnd5e.spells.TIoadMIsUKD4edXi", "Compendium.dnd5e.spells.XbwGq5kDJNvAxNXV", "Compendium.dnd5e.spells.Y6oItIuhOJZ0i0FC", "Compendium.dnd5e.spells.qLeEXZDbW5y4bmLY"
+			
+			]
+		},	
+		"value": 	{},
+		"level": 	17,
+		"title": 	"Spells - 9th Level",
+		"icon": 	"",
+		"classRestriction": 	""
+	},		
+	{		
+	"_id": 	"HgEYBPySrstzwWDI",	
+	"type": 	"ItemGrant",	
+	"configuration": 	{	
+		"items": 	[
+			
+			]
+		},	
+		"value": 	{},
+		"level": 	18,
+		"title": 	"Channel Divinity - 3/Rest",
+		"icon": 	"",
+		"classRestriction": 	""
+	},		
+	{		
+	"_id": 	"1QPNp39FyqTAf6HL",	
+	"type": 	"ItemGrant",	
+	"configuration": 	{	
+		"items": 	[
+			
+			]
+		},	
+		"value": 	{},
+		"level": 	20,
+		"title": 	"Divine Intervention - Automatic Success",
+		"icon": 	"",
+		"classRestriction": 	""
+	}		
+],			
